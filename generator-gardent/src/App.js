a414349f599e2a7310b150c29a05a2f1753c25a1
ZAKHARY Mina <85484181+MinaZA@users.noreply.github.com>
2022-03-18 17:31:35 +0100

    go

---
+++ generator-gardent/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { PhotoshopPicker } from "react-color";
+import gradient from "./gradient_generator";
+
+function App() {
+    return ( <
+        gradient / >
+    );
+}
+
+export default App;
